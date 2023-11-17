0f0c4ba9e9ed9c9660b0c92381c347ee86e415b9
ReziMi <revaz.mikadze92@gmail.com>
2023-11-17 14:53:23 +0400

    my first simple project with API

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const adviceID = document.getElementById("advice-id");
 const adviceTxt = document.getElementById("advice-text");
 const adviceBtn = document.getElementById("generate-btn");
-const url="https://api.adviceslip.com/advice";
 
-async function getAdvice(){
+const url = "https://api.adviceslip.com/advice";
+
+// Async function to fetch advice from the API
+async function getAdvice() {
+    // Fetch advice from the API
     const res = await fetch(url);
-    const {slip: {id, advice}}= await res.json();
-    adviceID.innerText=id;
-    adviceTxt.innerText=advice;
+
+    // Parse the JSON response and extract the id and advice properties
+    const { slip: { id, advice } } = await res.json();
+
+    adviceID.innerText = id;
+    adviceTxt.innerText = advice;
 }
 
 getAdvice();
